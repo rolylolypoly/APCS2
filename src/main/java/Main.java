d34faebe746ff4abf57956de73bee3105f2b083f
W Dang <wdang6867@golosal.net>
2015-10-06 08:10:56 -0700

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Author: Will Dang
+ * 10/5/2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String server = args[0];
+        String port = args[1];
+
+    }
+}
