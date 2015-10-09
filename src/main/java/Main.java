0940b2b1b4377be067d8462c986cfeb0209c88c8
W Dang <wdang6867@golosal.net>
2015-10-09 10:18:07 -0700

    Fahrenheit to Celsius. GPA calculator. Componematizer.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,56 @@
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Author: Will Dang
  * 10/5/2015.
  */
 public class Main {
-    public static void main(String[] args) {
-        String server = args[0];
-        String port = args[1];
+    static Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args)  {
+        componematizer();
+    }
+    public static void fahrenheitToCelsius() {
+        System.out.println("Fahrenheit : ");
 
+        int Fahrenheit = 0;
+
+        try {
+            Fahrenheit = keyboard.nextInt();
+        } catch (InputMismatchException e) {
+            System.err.println("Not a number");
+        }
+
+        int Celsius =  (Fahrenheit - 32) * 5 / 9;
+        System.out.println(Celsius + "C");
+    }
+
+    public static void gpaCalc() {
+
+        System.out.println("Enter 6 grade points");
+        System.out.println("First : ");
+        double grade1 = keyboard.nextDouble();
+        System.out.println("Second : ");
+        double grade2 = keyboard.nextDouble();
+        System.out.println("Third : ");
+        double grade3 = keyboard.nextDouble();
+        System.out.println("Fourth : ");
+        double grade4 = keyboard.nextDouble();
+        System.out.println("Fifth : ");
+        double grade5 = keyboard.nextDouble();
+        System.out.println("Sixth : ");
+        double grade6 = keyboard.nextDouble();
+
+        System.out.println("GPA: " + ((grade1 + grade2 + grade3 + grade4 + grade5 + grade6) / 6 ));
+
+    }
+    public static void componematizer() {
+        System.out.println("Radius: ");
+        double radius = keyboard.nextDouble();
+        System.out.println("Angle in degrees: ");
+        double theta = keyboard.nextDouble();
+        System.out.println("X:" + (radius * Math.cos(Math.toRadians(theta))));
+        System.out.println("Y:" + (radius * Math.sin(Math.toRadians(theta))));
     }
 }
