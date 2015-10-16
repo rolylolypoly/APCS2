bf069ca0f1cd9610bb9cb3921ca1fd3b587b8e35
rolylolypoly <rolylolypoly@users.noreply.github.com>
2015-10-15 21:15:47 -0700

    Reformat and switched to Scanner input.

--- src/main/java/CasesarCipher.java
+++ src/main/java/CasesarCipher.java
@@ -1,42 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /**
  * Author: Will Dang
  * 10/15/2015.
  */
 public class CasesarCipher {
-    public static void Cipher(int shift){
-        BufferedReader in
-                = new BufferedReader(new InputStreamReader(System.in));
-        try {
-
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println(line);
-                Shift(line, shift);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+    public static void Cipher(int shift) {
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        Shift(line, shift);
     }
+
     private static void Shift(String line, int shift) {
         line = line.toLowerCase();
         char[] output = new char[line.length()];
-        int i=0;
-        for (char ch: line.toCharArray()) {
+        int i = 0;
+        for (char ch : line.toCharArray()) {
             if (ch >= 97 && ch <= 122) {
-                output[i] = (char) (((ch - 97 + shift) % 26) +97);
-            }
-            else {
+                output[i] = (char) (((ch - 97 + shift) % 26) + 97);
+            } else {
                 output[i] = ch;
             }
             i++;
