68ed6fc93485ef9d54d454dc446f8d623ec6924f
W Dang <wdang6867@golosal.net>
2015-10-21 08:42:17 -0700

    Added dollars calculation to Coins.

--- src/main/java/Coins.java
+++ src/main/java/Coins.java
@@ -3,18 +3,32 @@
  * 10/20/2015.
  */
 public class Coins {
-    public static void yes(int cents) {
+    public static void yes(int cents, int dollars) {
+
+        int fiddy = dollars / 50;
+        dollars %= 25;
+        int twenties = dollars / 20;
+        dollars %= 20;
+        int ten = dollars / 10;
+        dollars %= 10;
+        int fives = dollars / 5;
+        dollars %= 5;
+
         int quarters = cents / 25;
         cents %= 25;
         int dimes = cents / 10;
         cents %= 10;
         int nickels = cents / 5;
         cents %= 5;
-        System.out.println(quarters);
-        System.out.println(dimes);
-        System.out.println(nickels);
-        System.out.println(cents);
-
+        System.out.println("Fiftie(s): " + fiddy);
+        System.out.println("Ten(s): " + ten);
+        System.out.println("Twentie(s): " + twenties);
+        System.out.println("Five(s): " + fives);
+        System.out.println("Dollar(s): " + dollars);
+        System.out.println("Quarter(s): " + quarters);
+        System.out.println("Dime(s): " + dimes);
+        System.out.println("Nickel(s): " + nickels);
+        System.out.println("Cent(s): " + cents);
 
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         System.out.println("Change: ");
-        Coins.yes(in.nextInt());
+        Coins.yes(in.nextInt(), in.nextInt());
     }
 }
