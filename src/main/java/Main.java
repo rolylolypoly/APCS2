3543442a91b0e6c30c07e65c01b3c7f5f7d73c5a
W Dang <wdang6867@golosal.net>
2015-10-21 09:19:40 -0700

    Doned and tested Post Office problem.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,11 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        System.out.println("Change: ");
-        Coins.yes(in.nextInt(), in.nextInt());
+        System.out.println("Dimmies: ");
+        double dim1 = in.nextDouble();
+        double dim2 = in.nextDouble();
+        double dim3 = in.nextDouble();
+        double weight = in.nextDouble();
+        PostOffice.Calculate(dim1, dim2, dim3, weight);
     }
 }
--- src/main/java/PostOffice.java
+++ src/main/java/PostOffice.java
@@ -4,10 +4,44 @@
  */
 public class PostOffice {
     public static void Calculate(double dim1, double dim2, double dim3, double weight) {
-        double[] yes = new double[2];
-        yes[0] = dim1;
-        yes[1] = dim2;
-        yes[3] = dim3;
-        
+        //double[] yes = new double[2];
+        //yes[0] = dim1;
+        //yes[1] = dim2;
+        //yes[3] = dim3;
+        double girth;
+        double lengthgirth;
+        if (dim1 > dim2 & dim2 > dim3) {
+            girth = dim2 * dim3;
+            lengthgirth = dim1 + girth;
+        }
+        else if (dim2 > dim1 & dim2 > dim3) {
+            girth = dim1 * dim3;
+            lengthgirth = dim2 + girth;
+        }
+        else if (dim3 > dim1 & dim3 > dim2) {
+            girth = dim1 * dim2;
+            lengthgirth = dim3 + girth;
+        }
+        else if (dim1 > dim2 & dim2 == dim3) {
+            girth = dim2 * dim3;
+            lengthgirth = dim1 + girth;
+        }
+        else if (dim2 > dim1 & dim1 == dim3) {
+            girth = dim1 * dim3;
+            lengthgirth = dim2 + girth;
+        }
+        else if (dim3 > dim1 & dim1 == dim2) {
+            girth = dim1 * dim2;
+            lengthgirth = dim3 + girth;
+        }
+        else {
+            girth = dim1 * dim2;
+            lengthgirth = dim1 + girth;
+            System.out.println("Cube. Or something. CHECK LOGIC.");
+        }
+        System.out.println(girth);
+        System.out.println(lengthgirth);
+        if (lengthgirth < 100.0 & weight < 70) System.out.println("Can ship.");
+        else System.out.println("Nope.");
     }
 }
