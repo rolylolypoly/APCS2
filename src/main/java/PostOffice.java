d5f8de5b92d6f9ba0765e9ec863ebcb793a0cb3a
W Dang <wdang6867@golosal.net>
2015-10-21 08:31:00 -0700

    Started Post Office problem.

---
+++ src/main/java/PostOffice.java
@@ -0,0 +1,13 @@
+/**
+ * Author: Will Dang
+ * 10/20/2015.
+ */
+public class PostOffice {
+    public static void Calculate(double dim1, double dim2, double dim3, double weight) {
+        double[] yes = new double[2];
+        yes[0] = dim1;
+        yes[1] = dim2;
+        yes[3] = dim3;
+        
+    }
+}
