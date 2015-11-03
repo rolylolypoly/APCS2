0ba7d6c064948ab1165cae5f6ca317c7ad595d32
Will Dang <dangwilliam2@gmail.com>
2015-11-03 08:37:18 -0800

    Finished CoolNumber lab.

---
+++ src/main/java/CoolNumber.java
@@ -0,0 +1,15 @@
+/**
+ * Created by rolypoly on 11/3/15.
+ */
+public class CoolNumber {
+    public static void is(int potato){
+        int count = 0;
+        for (int i = 6; i < potato; i++) {
+            if(isCoolNumber(i)) count ++;
+        }
+    }
+    public static boolean isCoolNumber(int i) {
+        if(i % 6 == 1 && i % 5 == 1 && i % 4 == 1) return true;
+        else return false;
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        long dong = input.nextLong();
+        CoolNumber.is(input.nextInt());
 
     }
 }
