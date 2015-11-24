5ef05b14ed581cc582a097ba1797d88e1811f6b6
Will <dangwilliam2@gmail.com>
2015-11-23 20:57:12 -0800

    Multithreaded the TCP client.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,48 +1,57 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by rolypoly on 11/23/15.
  */
-public class Client implements Runnable{
-    static {
-        String hostName = "localhost";
+public class Client implements Runnable {
+    private static Socket socket;
+
+    public static void init() throws IOException {
         int port = 56789;
-        try (
-                Socket socket = new Socket(hostName, port);
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream()))
-        ) {
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        String hostName = "localhost";
+        socket = new Socket(hostName, port);
     }
 
     @Override
     public void run() {
         (new Thread(new UserInput())).start();
         (new Thread(new ServerResponse())).start();
-
     }
 
     private class UserInput implements Runnable {
-
         @Override
         public void run() {
-
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()))
+            ) {
+                while (in.readLine() != null)
+                    System.out.println(in.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
-    private class ServerResponse implements Runnable {
 
+    private class ServerResponse implements Runnable {
         @Override
         public void run() {
-
+            try (
+                    PrintWriter out = new PrintWriter(socket.getOutputStream())
+            ) {
+                BufferedReader stdIn =
+                        new BufferedReader(new InputStreamReader(System.in));
+                while (stdIn.readLine() != null)
+                    out.println(stdIn.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
